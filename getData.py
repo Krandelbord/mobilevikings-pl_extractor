638a700d535f0822d2f031b1f9547d7cf89487b2
Emil <emil@japcok-pro.megasiec>
2017-10-22 00:34:18 +0200

    passing date range as arguments

--- getData.py
+++ getData.py
@@ -11,7 +11,7 @@ def begining_of_the_month():
     now = datetime.date.today()
     return datetime.date(now.year, now.month, 1)
 
-def extract_csv(username, password):
+def extract_csv(username, password, start_date, end_date):
     BASE_URL = 'https://mobilevikings.pl/pl'
     LOGIN_PAGE_URL = BASE_URL+'/account/login/'
     URL_TO_READ = BASE_URL+'/mysims/sim/51217/balance'
@@ -26,7 +26,8 @@ def extract_csv(username, password):
     r = client.post(LOGIN_PAGE_URL, data=login_data, headers=dict(Referer=LOGIN_PAGE_URL))
     print ("Login response is ", r)
     csv_response = client.get(BASE_URL+'/mysims/partials/51217/ng_history.html?export_format=csv&' +
-    'ordering=asc&start=2017-10-09&end=2017-10-16&type=usage&period=custom')
+    'ordering=asc&start='+start_date.strftime(DATE_FORMAT)+
+    '&end='+end_date.strftime(DATE_FORMAT)+'&type=usage&period=custom')
     csv_response.encoding = 'utf-8'
     out_csv_file_name = 'out.csv'
     with open(out_csv_file_name, 'w') as out_csv_file:
@@ -41,4 +42,4 @@ parser.add_argument('--start_date', help='start date to read history or 1st day 
 default=begining_of_the_month)
 
 args = parser.parse_args()
-extract_csv(args.user, args.password)
+extract_csv(args.user, args.password, args.start_date, datetime.date.today())
